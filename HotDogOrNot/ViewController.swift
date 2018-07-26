903ff28276480d56d63e0a88279487ea3ab911b3
Anand Nigam <anandnigam.2aug@gmail.com>
2018-07-26 15:35:49 +0530

    Initial Commit

---
+++ HotDogOrNot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotDogOrNot
+//
+//  Created by Anand Nigam on 26/07/18.
+//  Copyright © 2018 Anand Nigam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
