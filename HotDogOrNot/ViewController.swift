8141f0ee9443ade6e4846fddcfc04223110dbc61
Anand Nigam <anandnigam.2aug@gmail.com>
2018-08-04 17:24:32 +0530

    MLModel Inceptionv3 is been added. You can take the photo from your phone and the methods will tell you that it is a hotdog or not.

--- HotDogOrNot/ViewController.swift
+++ HotDogOrNot/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+       if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = pickedImage
+        
+        guard let ciImage = CIImage(image: pickedImage) else {
+            fatalError("Could not convert to CIImage")
+        }
+        
+        detectImage(image: ciImage)
+        
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
+    
+    
+    func detectImage( image: CIImage) {
+        
+        // Loading the Model
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading Core ML Model Failed")
+        }
+        
+        // Request to Model to classify the data passed to it
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process the image")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "HotDog!"
+                    
+                }
+                else {
+                    self.navigationItem.title = "Not HotDog!"
+                }
+            }
+            
+        }
+        
+        
+        // Handler use to complete the process of classifying the image
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request]) }
+        catch {
+            print(error)
+        }
+    }
+    
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+
+    }
+    
 
 }
 
